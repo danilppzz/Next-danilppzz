c24b65e463db290f3bf479e0da7a7621f89dc499
d4niz <josedanielmorebolopez@gmail.com>
2024-01-02 17:17:30 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,11 +10,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "danilppzz",
   description: "Portfolio of danilppzz!",
-  openGraph: {
-    title: "danilppzz",
-    images: "https://danilppzz.dev/icon.png",
-    description: "Portfolio of danilppzz!",
-  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -22,7 +17,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <meta name="og:type" content="website"></meta>
-        <meta name="og:color" content="#FFFFFF"></meta>
+        <meta name="og:title" content="danilppzz"></meta>
+        <meta name="og:description" content="Portfolio of danilppzz!"></meta>
+        <meta name="og:color" content="#ffffff"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
+        <meta name="og:image" content="https://danilppzz.dev/icon.png"></meta>
         <link rel="icon" href="/icon.png" type="image/png" sizes="128x128" />
       </head>
       <body className={inter.className}>
