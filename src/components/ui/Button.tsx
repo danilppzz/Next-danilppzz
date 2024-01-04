1f9d2beb7fb8d73b4e43e16f9c9e26f5c2674325
d4niz <josedanielmorebolopez@gmail.com>
2024-01-04 01:26:32 +0100

    responsive nav

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -13,23 +13,32 @@ interface ButtonProps {
   state?: "allow" | "deny";
   onClick?: () => void;
   children?: ReactNode;
+  className?: ReactNode;
 }
 
 const itim = Itim({ subsets: ["latin"], weight: "400" });
 
-const Button: React.FC<ButtonProps> = ({ icon, text, box, state, onClick, children }) => {
+const Button: React.FC<ButtonProps> = ({
+  icon,
+  text,
+  box,
+  state,
+  onClick,
+  children,
+  className,
+}) => {
   const buttonClasses = `text-light-20 py-2 px-4 rounded h-[35px] bg-dark-30 border-white border-solid border-opacity-20 border-[0.2px] rounded-4 flex items-center justify-center hover:bg-dark-10
-        ${state === "allow" ? "cursor-pointer" : "cursor-not-allowed"}
-        ${box ? "w-[35px]" : "w-[80px]"}
-    `;
+    ${state === "allow" ? "cursor-pointer" : "cursor-not-allowed"}
+    ${box ? "w-[35px]" : "w-[80px]"}
+  `;
 
   return (
     <button
-      className={itim.className + " " + buttonClasses}
+      className={buttonClasses + " " + itim.className}
       onClick={onClick}
       disabled={state === "deny"}
     >
-      {children}
+      {children && <div dangerouslySetInnerHTML={{ __html: children as string }} />}
     </button>
   );
 };
--- src/components/ui/HeaderNav.tsx
+++ src/components/ui/HeaderNav.tsx
@@ -1,16 +1,31 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Itim } from "next/font/google";
 import Button from "./Button";
-import LinkedingIcon from "../icons/linkedingIcon";
 
 interface HeaderNavProps {}
 
 const itim = Itim({ subsets: ["latin"], weight: "400" });
 
 const HeaderNav: React.FC<HeaderNavProps> = ({}) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   function openCV() {
     window.open("/cv", "_blank");
   }
@@ -28,27 +43,37 @@ const HeaderNav: React.FC<HeaderNavProps> = ({}) => {
           <Link href={"/"}>danilppzz</Link>
         </h1>
       </div>
-      <nav className="flex items-center space-x-4 h-[54px] desktop:mr-[300px] laptop:mr-2 tablet:mr-2">
-        <Link
-          className={`${itim.className} text-base text-light-20 text-[20px] transition duration-300 ease-in-out transform hover:scale-125`}
-          href="#about"
-        >
-          about
-        </Link>
-        <Link
-          className={`${itim.className} text-base text-light-20 text-[20px] transition duration-300 ease-in-out transform hover:scale-125`}
-          href="#projects"
-        >
-          projects
-        </Link>
-        <div className="border-l border-[#9D9D9D] h-6 mx-4" />
-        <Button state="allow" box={true} onClick={() => openCV()}>
-          cv
-        </Button>
-        <div className="border-l border-[#9D9D9D] h-6 mx-4" />
-        <Button state="allow" box={false}>
-          settings
-        </Button>
+      <nav className="flex items-center space-x-4 h-[54px] desktop:mr-[300px] laptop:mr-4 tablet:mr-4 mr-2">
+        {isMobile ? (
+          <>
+            <Button state="allow" box={true}>
+              {`<svg class="with-icon_icon__MHUeb" data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M3 12h18"/><path d="M3 6h18"/><path d="M3 18h18"/></svg>`}
+            </Button>
+          </>
+        ) : (
+          <>
+            <Link
+              className={`${itim.className} text-base text-light-20 text-[20px] transition duration-300 ease-in-out transform hover:scale-125`}
+              href="#about"
+            >
+              about
+            </Link>
+            <Link
+              className={`${itim.className} text-base text-light-20 text-[20px] transition duration-300 ease-in-out transform hover:scale-125`}
+              href="#projects"
+            >
+              projects
+            </Link>
+            <div className="border-l border-[#9D9D9D] h-6 mx-4" />
+            <Button state="allow" box={true} onClick={() => openCV()}>
+              cv
+            </Button>
+            <div className="border-l border-[#9D9D9D] h-6 mx-4" />
+            <Button state="allow" box={false}>
+              settings
+            </Button>
+          </>
+        )}
       </nav>
     </header>
   );
