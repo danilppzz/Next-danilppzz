3249ed2400bcdf6fc940e79e2c0c32e86818b07d
d4niz <josedanielmorebolopez@gmail.com>
2024-01-12 17:54:21 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import { Circle } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className=" bg-dark-10 h-[2005px] mt[60px] pt-[60px]">
-      <div className="mt-[220px]">
-        <h1 className=" text-light-20"> <LinkTag text="danilppzz" link="https://github.com/danilppzz"></LinkTag> </h1>
+    <main className=" bg-dark-10 h-[1005px] mt[60px] pt-[60px]">
+      <div className="flex justify-center items-center h-screen">
+      <h1 className=" text-light-20">
+          {" "}
+          <LinkTag text="danilppzz" link="https://github.com/danilppzz"></LinkTag>{" "}
+        </h1>
       </div>
     </main>
   );
