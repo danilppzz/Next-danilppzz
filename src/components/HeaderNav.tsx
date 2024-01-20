b9abd7574713bb544fd89710d0cc6ae3a65a5b6e
d4niz <josedanielmorebolopez@gmail.com>
2024-01-20 22:41:42 +0100

    Button & Colors

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -44,7 +44,7 @@ const HeaderNav: React.FC<HeaderNavProps> = ({}) => {
   };
 
   return (
-    <header className={`bg-dark-20 h-[60px] flex justify-between items-center fixed top-0 w-full z-10 bg-opacity-10 backdrop-blur-[2px] ${isMenuOpen ? "backdrop-blur-[10000px]" : ""} ${scrolled ? 'border-b-[0.5px] border-light-30 border-opacity-10' : 'border-b-[0.5px] border-light-30 border-opacity-0'}`}>
+    <header className={`bg-dark-10 h-[60px] flex justify-between items-center fixed top-0 w-full z-10 bg-opacity-10 backdrop-blur-[2px] ${isMenuOpen ? "backdrop-blur-[10000px]" : ""} ${scrolled ? 'border-b-[0.5px] border-light-30 border-opacity-10' : 'border-b-[0.5px] border-light-30 border-opacity-0'}`}>
       <div className="flex items-center space-x-4 desktop:ml-[400px] laptop-xl:ml-[200px] laptop:ml-2 tablet:ml-2">
         <Image
           className="select-none"
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -25,7 +25,7 @@ const Button: React.FC<ButtonProps> = ({
   children,
   className,
 }) => {
-  const buttonClasses = `text-light-20 py-2 px-4 rounded h-[35px] bg-dark-10 border-white border-solid border-opacity-20 border-[0.2px] rounded-4 flex items-center justify-center hover:bg-dark-30
+  const buttonClasses = `text-light-20 py-2 px-4 rounded h-[35px] bg-dark-10 border-light-30 border-solid border-opacity-10 border-[0.1px] rounded-4 flex items-center justify-center hover:bg-dark-20
     ${state ? "cursor-pointer" : "cursor-not-allowed"}
     ${box ? "w-[35px]" : "w-[80px]"}
   `;
