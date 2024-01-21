d5636a169a0d1fd6a26127bef78d50e195a2a383
d4niz <josedanielmorebolopez@gmail.com>
2024-01-22 00:31:29 +0100

    Menu Icon && Children Fix

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -76,7 +76,11 @@ const HeaderNav: React.FC<HeaderNavProps> = ({}) => {
         ) : isMobile ? (
           <>
             <Button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-              {`<svg class="with-icon_icon__MHUeb" data-testid="geist-icon" fill="none" height="24" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M3 12h18"/><path d="M3 6h18"/><path d="M3 18h18"/></svg>`}
+              {isMenuOpen === true ? (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" color="#fff" fill="#fff" height={20}><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" color="#fff" fill="#fff" height={17}><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+              )}
             </Button>
             {isMenuOpen && (
               <div className="absolute bg-dark-10 top-14 right-0 shadow-md p-2 w-full transition-all duration-1000 ease-in-out border-b-[0.5px] border-light-30 border-opacity-10">
