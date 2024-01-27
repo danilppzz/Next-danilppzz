0e258aa46264826549068ce2fae08db3ead2dd7c
d4niz <josedanielmorebolopez@gmail.com>
2024-01-27 23:40:27 +0100

    Metadata & Manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+        <meta property="theme-color" content="#ffffff"></meta>
         <meta property="og:type" content="website"></meta>
         <meta property="og:title" content="danilppzz"></meta>
-        <meta property="og:description" content="Portfolio of danilppzz!"></meta>
+        <meta property="og:description" content="Build awesome this with danilppzz!"></meta>
         <meta property="og:color" content="#ffffff"></meta>
-        <meta property="theme-color" content="#ffffff"></meta>
         <meta property="og:image" content="https://danilppzz.dev/icon.png"></meta>
+
+        <meta property="twitter:card" content="summary_large_image"></meta>
+        <meta property="twitter:title" content="danilppzz"></meta>
+        <meta property="twitter:description" content="Build awesome this with danilppzz!"></meta>
+        <meta property="twitter:color" content="#ffffff"></meta>
+        <meta property="twitter:image" content="https://danilppzz.dev/SRC_2.png"></meta>
+
         <link rel="icon" href="/icon.png" type="image/png" sizes="128x128" />
+        <link rel="manifest" href="/manifest.json"></link>
       </head>
       <body className={font.className}>
         <HeaderNav />
